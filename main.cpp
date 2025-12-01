7d58dd1a07f4255ff9f362993f1eb6139163d2a0
Diksha <diksha@macbookpro>
2025-11-30 19:08:07 -0800

    testing edits for m2

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@
 #include "IntBinaryTree.h"
 using namespace std;
 int main() {
+	//testing for milestone 2
+	IntBinaryTree bst;
+	bst.insertNode("xyz");
+	bst.insertNode("ghi");
+	bst.insertNode("def");
+	bst.insertNode("abc");
+	if (bst.searchNode("abc"));
+		cout << "'abc' found" << endl;
+	else
+		cout << "'abc' NOT found" << endl;
 	return 0;
 }
